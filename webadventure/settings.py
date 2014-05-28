d54a95348e541cdf1df79cc35647a23018f63365
Yuri Shakalov <yuriy.shakalov@gmail.com>
2014-05-28 22:20:44 +0300

    Added alembic

--- webadventure/settings.py
+++ webadventure/settings.py
@@ -1,25 +1,12 @@
 # Flask CSRF key
 SECRET_KEY = '123456790'
 
-# Google maps key
-GOOGLE_MAPS_KEY = 'AIzaSyD6ZKQ2MRcTtPnOHd71yHUljeaj1sNLcKM'
-
 # SQLAlchemy configuration
 SQLALCHEMY_DATABASE_URI = 'sqlite:///../data.sqlite'
-#SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:1@localhost/tvcb'
 SQLALCHEMY_ECHO = True
 
 # Site settings
 INDEX_VIEW = 'site.home'
-
-# Recaptcha
-RECAPTCHA_USE_SSL = True
-RECAPTCHA_PUBLIC_KEY = '6Ld77N0SAAAAAG-C3i3Oab5HkWdezs5t9gCYQDy4'
-RECAPTCHA_PRIVATE_KEY = '6Ld77N0SAAAAAD-OOp7rIRofPkrxeKG2iaOkhXtP'
-
-# Google
-GOOGLE_APP_KEY = '334068587572.apps.googleusercontent.com'
-GOOGLE_APP_SECRET = 'XQn51w4IASyod3cWTaDFmKGd'
 
 # Image uploads
 import os.path as op
